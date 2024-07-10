37fd0924edd2ffdbf6182ef15f6370406102ef03
VictorCiceia <victor.ciceia@enapsys.com>
2024-07-10 18:35:38 -0400

    JWT

--- src/main/java/com/api/school/controller/AuthController.java
+++ src/main/java/com/api/school/controller/AuthController.java
@@ -3,6 +3,8 @@ package com.api.school.controller;
 import com.api.school.dto.auth.AuthDto;
 import com.api.school.dto.auth.LoginDto;
 import com.api.school.dto.auth.RegisterDto;
+import com.api.school.service.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,14 +15,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/auth")
 public class AuthController {
 
-    @PostMapping(value = "login")
+    @Autowired
+    private AuthService authService;
+
+    @PostMapping(value = "/login")
     public ResponseEntity<AuthDto> login(@RequestBody LoginDto login){
-        return ResponseEntity.ok(new AuthDto("token"));
+        AuthDto authDto = this.authService.login(login);
+        return ResponseEntity.ok(authDto);
     }
 
     @PostMapping("/register")
     public ResponseEntity<AuthDto> register(@RequestBody RegisterDto dto){
-        return ResponseEntity.ok(new AuthDto("token"));
+        AuthDto authDto = this.authService.register(dto);
+        return ResponseEntity.ok(authDto);
     }
 
 }
--- src/main/java/com/api/school/entity/UserEntity.java
+++ src/main/java/com/api/school/entity/UserEntity.java
@@ -1,10 +1,17 @@
 package com.api.school.entity;
 
+import com.api.school.util.Role;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Document(collection = "users")
-public class UserEntity {
+public class UserEntity implements UserDetails {
 
     @Id
     private String id;
@@ -13,10 +20,16 @@ public class UserEntity {
 
     private String email;
 
-    public UserEntity(String id, String name, String email) {
+    private String password;
+
+    private Role role;
+
+    public UserEntity(String id, String name, String email, String password, Role role) {
         this.id = id;
         this.name = name;
         this.email = email;
+        this.password = password;
+        this.role = role;
     }
 
     public UserEntity(){
@@ -46,4 +59,52 @@ public class UserEntity {
     public void setEmail(String email) {
         this.email = email;
     }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority((role.name())));
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+
 }
